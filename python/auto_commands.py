0a6d278f9d8dab1a8c46cb45e3cf517bbc5e992f
tomteipl <tomteipl@gmail.com>
2025-03-01 14:54:09 +0100

    auto_commands.py 0.3: Add funtions
    Info in script

--- python/auto_commands.py
+++ python/auto_commands.py
@@ -13,7 +13,12 @@
 # GNU General Public License for more details.
 #
 #
+#[Change Log]
 #
+# 0.3   : Implemented the hook_completion_cb to provide autocompletion for stored commands.
+#       : Added Guides.
+#       : Added <del> by Index or String values.
+#       : Added "time" command to set timer for sending commands after start.
 #
 # 0.2   : added list, add, delete, clear commands
 #       : added save and load commands functions
@@ -23,10 +28,28 @@
 import weechat
 
 
-weechat.register("auto_commands", "Tomtei", "0.2", "GPL3", "Send auto commands on start", "", "")
+SCRIPT_NAME = "auto_commands"
+SCRIPT_AUTHOR = "Tomteipl"
+SCRIPT_VERSION = "0.3"
+SCRIPT_LICENSE = "GPL3"
+SCRIPT_DESC = "Send auto commands on start"
+
+weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", "")
 
 
-commands = []   # Add commands here
+#   Guides
+help = """
+    Usage:\n
+    /autocommands add <command> - adds command to the list. You can use spaces and special signs.
+    /autocommands del <number/string> - deletes command from the list. You can use /autocommands list to see the numbers or tap TAB for auto completion.
+    /autocommands list - shows the list of commands with index.
+    /autocommands clear - clears the list of commands.
+    /autocommands time <miliseconds> - 1sec = 1000ms, sets the timer for hook in miliseconds. Default value 10000ms = 10 sec.
+    """
+
+
+
+commands = []   # Commands are stored here
 
 def load_commands():
     global commands
@@ -37,19 +60,21 @@ def save_commands():
     weechat.config_set_plugin("commands", ",".join(commands))
 
 # adds commands to the list
-def add_command(_, buffer, args):
+def add_command(data, buffer, args):
     commands.append(args)
     save_commands()
     weechat.prnt(buffer, f"Command '{args}' added!")
     return weechat.WEECHAT_RC_OK
 
-def send_auto_commands(_, __):
+def send_auto_commands(data, buffer):
     for command in commands:
         weechat.command("", command)
         weechat.prnt("", f"Command '{command}' sent!")
     return weechat.WEECHAT_RC_OK
 
-def commands_cb(_, buffer, args):           # commands
+
+# [ ---COMMANDS--- ]
+def commands_cb(data, buffer, args):
     if args.startswith("list"):
         weechat.prnt(buffer, "Current commands: \n")
         for i, command in enumerate(commands):
@@ -57,24 +82,50 @@ def commands_cb(_, buffer, args):           # commands
         return weechat.WEECHAT_RC_OK
 
     elif args.startswith("add"):
-        add_command(_, buffer, args[len("add "):])
+        add_command(data, buffer, args[len("add "):])
         return weechat.WEECHAT_RC_OK
 
     elif args.startswith("del"):
         try:
-            index = int(args.split()[1]) -1
-            if 0 <= index < len(commands):
-                del_command = commands.pop(index)
+            arg_value = " ".join(args.split()[1:])
+
+            if arg_value.isdigit():         # delete command by index (example: /autocommands del 1)
+                index = int(args.split()[1]) -1
+
+                if 0 <= index < len(commands):
+                    del_command = commands.pop(index)
+                    save_commands()
+                    weechat.prnt(buffer, f"Command '{del_command}' deleted!")
+                    return weechat.WEECHAT_RC_OK
+
+                else:
+                    weechat.prnt(buffer, "Invalid command number!")
+                    return weechat.WEECHAT_RC_OK
+
+            elif arg_value in commands:          # delete command by string, you can use autocomplete (example: /autocommands del /join #channel)
+                commands.remove(arg_value)
                 save_commands()
-                weechat.prnt(buffer, f"Command '{del_command}' deleted!")
+                weechat.prnt(buffer, f"Command '{arg_value}' deleted!")
                 return weechat.WEECHAT_RC_OK
+            
             else:
-                weechat.prnt(buffer, "Invalid command number!")
+                weechat.prnt(buffer, "Invalid command number or string!")
                 return weechat.WEECHAT_RC_OK
 
         except (ValueError, IndexError):
                 weechat.prnt(buffer, "Invalid command number!")
                 return weechat.WEECHAT_RC_OK
+
+    elif args.startswith("time"):           # set timer for hook
+        try:
+            new_time = int(args.split()[1])
+            weechat.config_set_plugin("timer", str(new_time))
+            weechat.prnt(buffer, f"Timer set to {new_time} ms!")
+
+        except (ValueError, IndexError):
+            weechat.prnt(buffer, "Invalid time value! /autocommands time <miliseconds>")
+
+        return weechat.WEECHAT_RC_OK
 
     elif args.startswith("clear"):
         commands.clear()
@@ -82,10 +133,31 @@ def commands_cb(_, buffer, args):           # commands
         return weechat.WEECHAT_RC_OK
 
     else:
-        weechat.prnt(buffer, "Usage: /autocommands list|add|del (command number)")
+        weechat.prnt(buffer, f"{help}")
         return weechat.WEECHAT_RC_OK
+
+
+
+# Commands completion when using <del>
+def hook_completion_cb(data, completion, buffer, completion_item):
+    for command in commands:
+        weechat.hook_completion_list_add(completion_item, command, 0, weechat.WEECHAT_LIST_POS_SORT)
+    return weechat.WEECHAT_RC_OK
+
+
 
 load_commands()
 
-weechat.hook_command("autocommands", "List auto commands", "", "", "list || add  || del || clear", "commands_cb", "")
-weechat.hook_timer(10000, 0, 1, "send_auto_commands", "")
+weechat.hook_completion("autocommands_cmds", "List auto commands", "hook_completion_cb", "")
+weechat.hook_command(
+    "autocommands",
+    "List auto commands",
+    "",
+    "",
+    "list || add  || del %(autocommands_cmds) || clear || time",
+    "commands_cb",
+    "",
+)
+
+timer = int(weechat.config_get_plugin("timer") or 10000)
+weechat.hook_timer(timer, 0, 1, "send_auto_commands", "")
