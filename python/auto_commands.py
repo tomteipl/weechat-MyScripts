20d1669d2bf019a2bdc24462141f6eea56c5104d
tomtei <tomteipl@gmail.com>
2025-03-01 02:29:51 +0100

    auto_commands.py 0.2: Update
    Info in script

--- python/auto_commands.py
+++ python/auto_commands.py
@@ -12,31 +12,44 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# 0.1 - Initial release
+#
+#
+#
+# 0.2   : added list, add, delete, clear commands
+#       : added save and load commands functions
+#
+# 0.1   : Initial release
 
 import weechat
-from time import sleep
 
 
-weechat.register("auto_commands", "Tomtei", "0.1", "GPL3", "Send auto commands on start", "", "")
+weechat.register("auto_commands", "Tomtei", "0.2", "GPL3", "Send auto commands on start", "", "")
+
 
 commands = []   # Add commands here
 
+def load_commands():
+    global commands
+    saved_commands = weechat.config_get_plugin("commands")
+    commands = saved_commands.split(",") if saved_commands else []
+
+def save_commands():
+    weechat.config_set_plugin("commands", ",".join(commands))
 
 # adds commands to the list
 def add_command(_, buffer, args):
     commands.append(args)
+    save_commands()
     weechat.prnt(buffer, f"Command '{args}' added!")
     return weechat.WEECHAT_RC_OK
 
-def send_auto_commands():
+def send_auto_commands(_, __):
     for command in commands:
         weechat.command("", command)
         weechat.prnt("", f"Command '{command}' sent!")
-        sleep(2)
     return weechat.WEECHAT_RC_OK
 
-def commands_cb(_, buffer, args):
+def commands_cb(_, buffer, args):           # commands
     if args.startswith("list"):
         weechat.prnt(buffer, "Current commands: \n")
         for i, command in enumerate(commands):
@@ -44,21 +57,35 @@ def commands_cb(_, buffer, args):
         return weechat.WEECHAT_RC_OK
 
     elif args.startswith("add"):
-        add_command(_, buffer, args[len("add"):])
+        add_command(_, buffer, args[len("add "):])
         return weechat.WEECHAT_RC_OK
 
     elif args.startswith("del"):
-        del_command = args[len("del"):]
-        if del_command in commands:
-            commands.remove(del_command)
-            weechat.prnt(buffer, f"Command '{del_command}' removed!")
-        else:
-            weechat.prnt(buffer, f"Command '{del_command}' not found!")
+        try:
+            index = int(args.split()[1]) -1
+            if 0 <= index < len(commands):
+                del_command = commands.pop(index)
+                save_commands()
+                weechat.prnt(buffer, f"Command '{del_command}' deleted!")
+                return weechat.WEECHAT_RC_OK
+            else:
+                weechat.prnt(buffer, "Invalid command number!")
+                return weechat.WEECHAT_RC_OK
+
+        except (ValueError, IndexError):
+                weechat.prnt(buffer, "Invalid command number!")
+                return weechat.WEECHAT_RC_OK
+
+    elif args.startswith("clear"):
+        commands.clear()
+        weechat.prnt(buffer, "Commands cleared!")
         return weechat.WEECHAT_RC_OK
 
     else:
-        weechat.prnt(buffer, "Usage: /autocommands list|add|del")
+        weechat.prnt(buffer, "Usage: /autocommands list|add|del (command number)")
         return weechat.WEECHAT_RC_OK
 
-weechat.hook_command("autocommands", "List auto commands", "", "", "list|add|del", "commands_cb", "")
+load_commands()
+
+weechat.hook_command("autocommands", "List auto commands", "", "", "list || add  || del || clear", "commands_cb", "")
 weechat.hook_timer(10000, 0, 1, "send_auto_commands", "")
